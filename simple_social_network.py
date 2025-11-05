93e58aa64328fc260ca19d1dde2dc6fc80e97d75
Averiel C <coakleya@terpmail.umd.edu>
2025-11-05 17:06:51 -0500

    added simple_social_network.py

---
+++ simple_social_network.py
@@ -0,0 +1,10 @@
+""" This is a simple social network for tracking connections between people."""
+
+class Person: 
+    """A person in the social network. 
+    
+        Attributes: 
+                name(str): the persons name.
+                connections (set of Person): other people in the social network
+                    who knows this person. 
+    """
